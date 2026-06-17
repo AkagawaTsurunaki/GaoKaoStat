b82e1948c5fac7265ec29fc8af33b0f04f135ded
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2026-06-17 11:04:39 +0800

    feat: 修整了主函数

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from pathlib import Path
 
 from plot import draw_dist_plot, draw_single, draw_scatter_plot
-from stat_analyse import compare_num_people
+from stat_analyse import compare_moment, compare_num_people, compare_other_index
 from stat_analyse import run_stat_tasks_with_multiprocess, run_stat_tasks
 
 
 def main():
     path_list = [path.absolute() for path in Path('./data').rglob("*_2025*.csv")]
-    # path_list = [Path('./data/江西_2021_历史类.csv')]
     result = run_stat_tasks(path_list)
-    # compare_num_people(result)
-    # compare_phyhis_ratio(result)
-    # compare_other_index(result)
-    # draw_single(result[0], title="2021年 江西(文科)")
+    # result = run_stat_tasks_with_multiprocess(path_list)
+    
+    # 这些是直接在命令行输出的
+    compare_num_people(result)
+    compare_other_index(result)
+    compare_moment(result)
+    
+    # 带 draw 开头的是画图的函数
     draw_dist_plot(result)
-    # draw_scatter_plot(result)
-    # draw_scatter_plot(result)
-    # compare_skewness(result)
+    draw_scatter_plot(result)
 
+    # 想只输出某个省市的就用下面这个
+    # path_list = [Path('./data/江西_2021_历史类.csv')]
+    # draw_single(result[0], title="2021年 江西(文科)")
+    
+    print('Done')
 
 if __name__ == "__main__":
     main()
